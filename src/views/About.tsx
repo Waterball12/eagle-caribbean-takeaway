3fd0164049f4e5c1f6ac0c9c00380be04def32b0
Waterball <waterballs12@gmail.com>
2021-02-25 00:11:43 +0000

    Partially redesign about us page

--- src/views/About.tsx
+++ src/views/About.tsx
@@ -1,121 +1,152 @@
 import React from 'react';
-import {Box, Container, Grid, Typography} from "@material-ui/core";
+import {Box, Container, Grid, Theme, Typography} from "@material-ui/core";
 import Image from "../components/Image";
 import {makeStyles} from "@material-ui/core/styles";
-import HeroHeader from "../assets/svg/About/Presentation attributes/Group 47.svg";
-import ManBlobAbout from "../assets/images/BuildingGraphicAboutUs.jpg";
 import ManBlobPresentation from "../assets/svg/About/Presentation attributes/Group 50.svg"
 import LadyBlobAbout from "../assets/svg/About/Presentation attributes/Group 49.svg";
-import Divider from "../assets/svg/About/Presentation attributes/Group 54.svg"
-import FindUseImage from "../assets/svg/About/Presentation attributes/Group 52.svg"
-import GoogleMapsPlaceholder from "../assets/svg/About/Presentation attributes/Image 1.svg";
 import Footer from "../components/Footer";
+import Button from "@material-ui/core/Button";
+import ShopOutsideImage from "../assets/images/eagle_outside.jpg";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 
-const useStyles = makeStyles(() => ({
-    elm: {
-        "backgroundRepeat": "no-repeat",
-        "position": "absolute",
-        "overflow": "hidden",
-        "top": '-46%',
-        "left": 0,
-        "width": "100%",
-        "height": "100%",
-        backgroundSize: 'contain',
-        "display": "flex !important",
-        "alignItems": "center !important"
+const useStyles = makeStyles((theme: Theme) => ({
+    heroSection: {
+        padding: `${theme.spacing(1)}px`,
+        [theme.breakpoints.up('md')]: {
+            paddingTop: '40px',
+            padding: '96px 64px',
+            margin: "0 auto",
+            maxWidth: '1236px',
+            overflow: 'hidden'
+        }
     },
-    root: {
-        "display": "block",
-        "paddingTop": "147.188px",
-        "minHeight": "780px",
-        "overflow": "hidden",
-        "background": "transparent",
-        "position": "relative",
-        "zIndex": 0
-    },
-    next: {
-        width: '100%',
-        display: 'block',
-        maxWidth: '100%',
-        minHeight: '1px',
-        margin: 0,
-    },
+    heroHeadingContainer: {
+        padding: `${theme.spacing(3)}px ${theme.spacing(2)}px`,
+        [theme.breakpoints.up('md')]: {
+            paddingTop: `${theme.spacing(10)}px`
+        }
+    }
 }))
 
 const About = () => {
     const classes = useStyles();
+
+    const isMobile = useMediaQuery((theme: Theme) => theme.breakpoints.down('sm'))
+
     return (
         <React.Fragment>
-            <section className={classes.root}>
-                <div className={classes.elm}>
-                    <figure className={classes.next}>
-                        <img style={{height: '500px', width: '100%'}} src={HeroHeader} alt="Blob" />
-                    </figure>
+            <section className={classes.heroSection}>
+                <div>
+                    <Grid container spacing={2} direction={isMobile ? "column-reverse" : undefined}>
+                        <Grid item xs={12} md={6}>
+                            <Grid container spacing={2} className={classes.heroHeadingContainer}>
+                                <Grid item xs={12}>
+                                    <Typography variant="subtitle1" color="primary" >
+                                        About us
+                                    </Typography>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Typography variant="h3" color="textPrimary" style={{fontWeight: 'bold'}}>
+                                        Eagle caribbean and english takeaway
+                                    </Typography>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Typography variant="caption" color="textSecondary">
+                                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+                                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+                                    </Typography>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Button color="primary" variant="contained" size="large">
+                                        See the chiefs
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                        <Grid item xs={12} md={6}>
+                            <Image src={ShopOutsideImage} />
+                        </Grid>
+                    </Grid>
                 </div>
-                <Box>
-                    <div style={{width: '100%', height: '100%', position: 'absolute', top: '40%', left: '10%'}}>
-                        <Typography variant="h2" color="primary" style={{fontWeight: 700}}>
-                            ABOUT US
-                        </Typography>
-                    </div>
-                    <Image variant="square" src={ManBlobAbout} alt="Hero image" />
-                </Box>
             </section>
-            <section style={{marginTop: '24px'}}>
+
+            <section style={{width: '100%', height: '100%', position: 'relative'}}>
                 <Container>
-                    <Grid container spacing={3} alignItems="center">
-                        <Grid item xs={12} lg={6}>
-                            <Image src={LadyBlobAbout} alt="rectangle" />
+                    <Grid container spacing={4}>
+                        <Grid item xs={12} sm={12} md={6}>
+                            <Image src={LadyBlobAbout} />
                         </Grid>
-                        <Grid item xs={12} lg={6}>
-                            <Typography variant="h2" color="primary" gutterBottom>
-                                Some title
-                            </Typography>
-                            <Typography variant="body2" color="textSecondary">
-                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unk
-                                nown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, rema
-                                ining essentially unchanged. It was popularised in the 1960s with the release o
-                                f Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                            </Typography>
-                        </Grid>
-                    </Grid>
-                </Container>
-            </section>
-            <section style={{marginTop: '5%', marginBottom: '5%'}}>
-                <Container>
-                    <Box display="flex" justifyContent="center" height="100%">
-                        <Image src={Divider} alt="Divider" />
-                    </Box>
-                </Container>
-            </section>
-            <section>
-                <Container>
-                    <Grid container spacing={3}>
-                        <Grid item xs={12} lg={6}>
-                            <Typography variant="h2" color="primary" gutterBottom style={{marginTop: '23%'}}>
-                                Some title
-                            </Typography>
-                            <Typography variant="body2" color="textSecondary">
-                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unk
-                                nown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, rema
-                                ining essentially unchanged. It was popularised in the 1960s with the release o
-                                f Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={12} lg={6}>
-                            <Image src={ManBlobPresentation} alt="rectangle" />
+                        <Grid item xs={12} sm={12} md={6}>
+                            <Grid container spacing={2} className={classes.heroHeadingContainer}>
+                                <Grid item xs={12}>
+                                    <Typography variant="subtitle1" color="primary" >
+                                        Jane Doe
+                                    </Typography>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Typography variant="h4" color="textPrimary" style={{fontWeight: 'bold'}}>
+                                        Get to know how the manager
+                                    </Typography>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Typography variant="caption" color="textSecondary">
+                                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+                                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+                                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+                                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+                                    </Typography>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Box mt={4}>
+                                        <Button color="primary" variant="contained" size="large">
+                                            Explore our story
+                                        </Button>
+                                    </Box>
+                                </Grid>
+                            </Grid>
                         </Grid>
                     </Grid>
                 </Container>
             </section>
-            <section>
+            <section style={{width: '100%', height: '100%', position: 'relative'}}>
                 <Container>
-                    <Box position="relative" width="100%">
-                        <Image src={FindUseImage} alt="Find us" />
-                    </Box>
-                    <Box position="relative" width="100%" mt="48px">
-                        <Image src={GoogleMapsPlaceholder} alt="Google maps" />
-                    </Box>
+                    <Grid container spacing={4}>
+                        <Grid item xs={12} sm={12} md={6}>
+                            <Grid container spacing={2} className={classes.heroHeadingContainer}>
+                                <Grid item xs={12}>
+                                    <Typography variant="subtitle1" color="primary" >
+                                        John Doe
+                                    </Typography>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Typography variant="h4" color="textPrimary" style={{fontWeight: 'bold'}}>
+                                        Get to know how the chief
+                                    </Typography>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Typography variant="caption" color="textSecondary">
+                                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+                                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+                                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+                                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+                                    </Typography>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Box mt={4}>
+                                        <Button color="primary" variant="contained" size="large">
+                                            Explore our story
+                                        </Button>
+                                    </Box>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                        <Grid item xs={12} sm={12} md={6}>
+                            <Image src={ManBlobPresentation} />
+                        </Grid>
+                    </Grid>
                 </Container>
             </section>
             <Footer />
